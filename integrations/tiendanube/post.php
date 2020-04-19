315ae49166fe653aebbc4ce679b314fe18b778ec
SocialRootio <60907568+SocialRootio@users.noreply.github.com>
2020-04-19 19:14:03 -0300

    Update post.php

--- integrations/tiendanube/post.php
+++ integrations/tiendanube/post.php
@@ -6,10 +6,10 @@ include('../../assets/libraries/Requests.php');
 // Next, make sure Requests can load internal classes
 Requests::register_autoloader();
 
-
+$mCode = "acavaelcodedeTN";
 
 //Seteamos el cuerpo del mensaje que vamos a enviar luego
-$DataBody = array(
+$data = array(
         'code' => $mCode, // Le mandamos el code que recibimos antes
         'grant_type' => "authorization_code", // Esto lo requiere TiendaNube
         'client_secret' => "CRLWSO1XkkV7OlZ65bHFLSzPS0g6EwAbVQuDmYMWPIwiYWrW", // CLient_secret de la app
@@ -17,16 +17,15 @@ $DataBody = array(
 );
 
 // Avisamos que vamos a enviar contenido en jSon. y contamos los caracteres de $payload
-
+$headers = array(
+    'Content-Type' => 'application/json'
+);
 
 $url = 'https://socialroot.requestcatcher.com/';
 $headers = array('Content-Type' => 'application/json');
 
-
-
-
 // Now let's make a request!
-$request = Requests::post($url, $headers, $Body);
+$request = Requests::post($url, $headers, $data);
 
 // Check what we received
 var_dump($request);
